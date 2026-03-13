a7639b8c59cb1ae37480ec3c75afe2947a266fca
lexiforman <alforman@chapman.edu>
2026-03-12 21:58:32 -0700

    wrote outline for getting score, setting a new score, and setting a highscore

---
+++ Scripts/Score.cs
@@ -0,0 +1,29 @@
+namespace DefaultNamespace;
+
+public class Score
+{
+    public static getScore()
+    {
+        // create score variable
+        // receive input from dice1 & dice2
+        // set score variable to the sum of dice1 & dice2
+        // return score
+    }
+
+    public static void setScore(int score)
+    {
+        //getScore ()
+        //change text in Unity to new score
+        // if player rolls poison dice, setScore = 0
+    }
+    
+    public static void setHighscore(int score)
+    {
+        //start with highscore = 0
+        //if getScore () > highscore, change highscore to getScore()
+    }
+
+    
+    
+    
+}
