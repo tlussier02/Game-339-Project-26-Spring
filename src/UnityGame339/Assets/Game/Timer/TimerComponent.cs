3ab3f2df79a200afcb1de2ac48c43c18087c41f2
lihong44 <lihong@chapman.edu>
2026-04-27 14:44:23 -0700

    tested in unity scene; it works!
    added a c# action event too for when it stops. i didnt add any state machine functionality since idk when thatll be done.

--- src/UnityGame339/Assets/Game/Timer/TimerComponent.cs
+++ src/UnityGame339/Assets/Game/Timer/TimerComponent.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
 using Game339.Shared.Services;
+using TMPro;
 
 namespace Game
 {
     public class TimerComponent : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _timerText;
+        
         private Game339.Shared.Services.Timer _timer;
-
         public float roundTimer = 60f; 
+        public event Action OnTimerFinished;
 
         private void Awake()
         {
@@ -18,7 +22,12 @@ namespace Game
         private void Update()
         {
             _timer.Tick();
-            Debug.Log(_timer.Current);
+            _timerText.text = Mathf.CeilToInt(_timer.Current).ToString();
+            if(_timer.Current <= 0)
+            {
+                OnTimerFinished?.Invoke();  // 👈 "hey anyone listening, timer is done!"
+            }
         }
+        
     }
 }
