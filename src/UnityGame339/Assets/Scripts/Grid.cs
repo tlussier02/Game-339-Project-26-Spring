355de2e67617df463a8ef056b5d0d943e34d9d47
lexiforman <alforman@chapman.edu>
2026-04-28 12:49:02 -0700

    Added population

---
+++ src/UnityGame339/Assets/Scripts/Grid.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+    public Tilemap Tilemap;
+    // convert from screen position to grid coordinates
+    //public Vector2Int ScreenToGrid(Vector2 screenPosition)
+   // {
+    //}
+    // convert from world to grid
+    // conver from grid to world
+}
---
+++ src/UnityGame339/Assets/Scripts/GridPopulator.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridPopulator : MonoBehaviour
+{
+    public Vector3Int gridStartCell = new  Vector3Int(0, 5, 0);
+    public Tilemap Tilemap;
+    public List<TileBase> TilesToPlace;
+    
+    public void Start()
+    {
+        for (int x = 0; x <= 8; x++)
+        {
+            for (int y = 0; y <= 8; y++)
+            {
+                Vector3Int cellPosition = gridStartCell + new Vector3Int(x, -y, 0);
+                Tilemap.SetTile(cellPosition, TilesToPlace[Random.Range(0, TilesToPlace.Count)]);
+            }
+        }
+    }
+}
